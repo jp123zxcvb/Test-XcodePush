98124e1c3b1382779685c0bc389a64b6b36ac60d
Frank <807084465@qq.com>
2026-04-23 11:36:09 -0700

    Initial Commit

---
+++ Test01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test01
+//
+//  Created by Frank on 2026/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test01/Test01App.swift
@@ -0,0 +1,17 @@
+//
+//  Test01App.swift
+//  Test01
+//
+//  Created by Frank on 2026/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
